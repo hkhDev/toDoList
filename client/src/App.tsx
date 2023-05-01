e33e4e7e9848654d81b60255673e6dd7cffd812b
Harris Ho <>
2023-05-01 15:27:25 -0700

    client update

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home";
+import ToDoList from "./components/ToDoList";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todolist" element={<ToDoList />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
